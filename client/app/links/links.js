9ee7868ebb95ac0c69eacce76a72f8afc2f1f9c3
juaniiie <becjuana@gmail.com>
2015-09-18 17:54:23 -0700

    Initial commit.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,33 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {
+    links: []
+    // url: '',
+    // base_url: '',
+    // code: '',
+    // title: '',
+    // visits: ''
+  };
+  // $scope.Links = [];
+  //save links
+
+  $scope.saveLinks = function () {
+    
+
+
+  };
+  //fetch links
+  $scope.getLinks = function () {
+   //references all links in linkController.js
+   console.log('============getlinks CAlled=======');
+    Links.getLinks($scope.data)
+    .then(function () {
+      $location.path('/links');
+    })
+    .catch(function (error) {
+        console.error(error);
+      });
+  };
+  //nav to links
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,9 +1,40 @@
 angular.module('shortly.services', [])
 
-.factory('Links', function ($http) {
+.factory('Links', function($http) {
   // Your code here
+  //ROBERT: call functions from links controller for GET/POST requests such as
+  //save links
+
+  var saveLinks = function(link) {
+    return $http({
+      method: 'POST',
+      url: '/', //May need api here part of path
+      data: link
+
+    })
+
+  };
+  //fetch links
+  //nav to link
+  var getLinks = function(link) {
+    return $http({
+        method: 'GET',
+        url: '/api/links', //May need api here part of path
+        data: link
+
+      })
+      .then(function(resp) {
+        console.log('============resp.data==========', ∏∏∏∏∏∏resp.data);
+        return resp.data.links;
+      });
+  };
+  return {
+    getLinks: getLinks,
+    saveLinks: saveLinks
+  };
 })
-.factory('Auth', function ($http, $location, $window) {
+
+.factory('Auth', function($http, $location, $window) {
   // Don't touch this Auth service!!!
   // it is responsible for authenticating our user
   // by exchanging the user's username and password
@@ -11,37 +42,36 @@ angular.module('shortly.services', [])
   // that JWT is then stored in localStorage as 'com.shortly'
   // after you signin/signup open devtools, click resources,
   // then localStorage and you'll see your token from the server
-  var signin = function (user) {
+  var signin = function(user) {
     return $http({
-      method: 'POST',
-      url: '/api/users/signin',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
+        method: 'POST',
+        url: '/api/users/signin',
+        data: user
+      })
+      .then(function(resp) {
+        return resp.data.token;
+      });
   };
 
-  var signup = function (user) {
+  var signup = function(user) {
     return $http({
-      method: 'POST',
-      url: '/api/users/signup',
-      data: user
-    })
-    .then(function (resp) {
-      return resp.data.token;
-    });
+        method: 'POST',
+        url: '/api/users/signup',
+        data: user
+      })
+      .then(function(resp) {
+        return resp.data.token;
+      });
   };
 
-  var isAuth = function () {
+  var isAuth = function() {
     return !!$window.localStorage.getItem('com.shortly');
   };
 
-  var signout = function () {
+  var signout = function() {
     $window.localStorage.removeItem('com.shortly');
     $location.path('/signin');
   };
-
 
   return {
     signin: signin,
